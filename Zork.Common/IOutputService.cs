9f782917671121fcb587ea379c582f8746a1ae8a
Eric O <ericdanielosorio@gmail.com>
2021-11-20 20:14:54 -0500

    Added dependencies

---
+++ Zork.Common/IOutputService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zork.Common
+{
+    interface IOutputServices
+    {
+        void Write(object value);
+
+        void WriteLine(object value);
+    }
+}
